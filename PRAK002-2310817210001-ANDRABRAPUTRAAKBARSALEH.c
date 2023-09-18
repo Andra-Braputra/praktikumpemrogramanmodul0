b9c3b9baabaaebfb4bf895a242ff23221911dd28
Andra <aandraaa24@gmail.com>
2023-09-18 12:22:03 +0800

    first commit

---
+++ PRAK002-2310817210001-ANDRABRAPUTRAAKBARSALEH.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main () {
+    int a;
+    printf("Selamat Pagi, Andra Braputra Akbar Saleh\n");
+    printf("Selamat Siang, Andra Braputra Akbar Saleh\n");
+    printf("Selamat Malam, Andra Braputra Akbar Saleh\n");
+    return 0;
+}
---
+++ PRAK004-2310817210001-ANDRABRAPUTRAAKBARSALEH.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main () {
+    int a;
+    printf("##############################\n");
+    printf("#                            #\n");
+    printf("# Andra Braputra Akbar Saleh #\n");
+    printf("#     2310817210001          #\n");
+    printf("#                            #\n");
+    printf("##############################\n");
+    return 0;
+}
